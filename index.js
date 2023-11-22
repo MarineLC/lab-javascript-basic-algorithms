19580be6cf848cc5a121f5603b6fe98b344daeae
MarineLC <49683302+MarineLC@users.noreply.github.com>
2023-11-22 15:45:16 +0100

    Updateiteration1

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ console.log(`The navigator's name is ${hacker2}`)
 if(hacker1.length > hacker2.length){
   console.log(`The driver has the longest name, it has ${hacker1.length} characters`);
 }else if(hacker1.length < hacker2.length){
-  console.log(`The navigator has the longest name, it has ${hacker1.length} characters`);
+  console.log(`The navigator has the longest name, it has ${hacker2.length} characters`);
 }else if(hacker1.length === hacker2.length){
   console.log(`Wow, you both have equally long names, ${hacker1.length} characters!`);
 }
